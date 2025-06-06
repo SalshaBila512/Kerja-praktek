67e07a651b96524d590c1ffaaff044e5f7ac4c2c
SalshaBila512 <Salshabilatiara512@gmail.com>
2025-06-06 21:11:11 +0700

    Add files via upload

---
+++ guru/dashboard.php
@@ -0,0 +1,185 @@
+<?php
+require 'function.php';
+require 'cek.php';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Start: Icons -->
+    <link href="https://cdn.jsdelivr.net/npm/remixicon@2.5.0/fonts/remixicon.css" rel="stylesheet">
+    <!-- Start: Icons -->
+    <!-- Start: CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <link rel="stylesheet" href="./assets/css/admin.css">
+    <!-- End: CSS -->
+    <title>Dashboard Admin Taud Rabbani Depok</title>
+</head>
+
+<body>
+
+    <!-- start: Sidebar -->
+    <div class="sidebar position-fixed top-0 bottom-0 bg-white border-end">
+        <div class="d-flex align-items-center p-3">
+            <a href="admin.php" class="sidebar-logo text-uppercase fw-bold text-decoration-none text-indigo fs-2">Taud Rabbani Depok</a>
+        </div>
+        <ul class="sidebar-menu p-3 m-0 mb-0">
+            <li class="sidebar-menu-item active">
+                <a href="admin.php">
+                    <i class="ri-dashboard-line sidebar-menu-item-icon"></i>
+                    Dashboard
+                </a>
+            </li>
+            <li class="sidebar-menu-divider mt-3 mb-1 text-uppercase">Data Sekolah</li>
+            <!-- Data Guru Start -->
+            <li class="sidebar-menu-item">
+                <a href="guru.php">
+                    <i class="ri-user-line sidebar-menu-item-icon"></i>
+                    Data Guru
+                </a>
+            </li>
+            <!-- Data Guru End -->
+
+            <!-- Data Siswa Start -->
+            <li class="sidebar-menu-item">
+                <a href="siswa.php">
+                    <i class="ri-user-line sidebar-menu-item-icon"></i>
+                    Data Siswa
+                </a>
+            </li>
+            <!-- Data Siswa End -->
+
+            <!-- Fasilitas Start -->
+            <li class="sidebar-menu-item">
+                <a href="fasilitas.php">
+                    <i class="ri-star-line sidebar-menu-item-icon"></i>
+                    Fasilitas
+                </a>
+            </li>
+            <!-- Fasilitas End -->
+
+            <!-- Events Start -->
+            <li class="sidebar-menu-divider mt-3 mb-1 text-uppercase">Events</li>
+            <li class="sidebar-menu-item">
+                <a href="berita.php">
+                    <i class="ri-article-line sidebar-menu-item-icon"></i>
+                    Berita
+                </a>
+            </li>
+            <li class="sidebar-menu-item">
+                <a href="galeri.php">
+                    <i class="ri-gallery-line sidebar-menu-item-icon"></i>
+                    Galeri
+                </a>
+            </li>
+
+            <!-- Kelola User Start -->
+            <li class="sidebar-menu-divider mt-3 mb-1 text-uppercase">Users</li>
+            <li class="sidebar-menu-item">
+                <a href="users.php">
+                    <i class="ri-user-settings-fill sidebar-menu-item-icon"></i>
+                    Kelola Users
+                </a>
+            </li>
+        </ul>
+    </div>
+    <div class="sidebar-overlay"></div>
+    <!-- End: Sidebar -->
+
+    <!-- Start: Main -->
+    <main class="bg-light">
+        <div class="p-2">
+            <!-- Start: Navbar -->
+            <nav class="px-3 py-2 bg-white rounded shadow-sm">
+                <i class="ri-menu-line sidebar-toggle me-3 d-block d-md-none"></i>
+                <h5 class="fw-bold mb-0 me-auto">Dashboard</h5>
+                <!-- Admin Info Start -->
+                <div class="dropdown">
+                    <div class="d-flex align-items-center cursor-pointer dropdown-toggle" data-bs-toggle="dropdown" aria-expanded="false">
+                        <span class="me-2 d-none d-sm-block">Admin</span>
+                        <a class="nav-link cursor-pointer dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="ri-map-pin-user-fill"></i></a>
+                    </div>
+                    <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                        <li><a class="dropdown-item" href="logout.php">Logout</a></li>
+                    </ul>
+                </div>
+                <!-- Admin Info End -->
+            </nav>
+            <!-- End: Navbar -->
+
+            <!-- Start: Content -->
+            <div class="py-4">
+                <!-- Start: Summary -->
+                <div class="container-fluid">
+                    <h1 class="mt-4">Selamat Datang Kembali!</h1>
+                    <p>Pada halaman ini, Anda dapat melakukan editing data sekolah</p>
+
+                    <div class="row">
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-primary text-white mb-4">
+                                <div class="card-body"><i class="ri-user-line "></i> Data Guru : <?php echo number_format($jmlh_data_guru,0,",",".");?></div>
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <a class="small text-white stretched-link" href="guru.php">View Details</a>
+                                    <div class="small text-white"><i class="ri-arrow-right-s-line"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-warning text-white mb-4">
+                                <div class="card-body"><i class="ri-user-line"></i> Data Siswa : <?php echo number_format($jmlh_data_siswa,0,",",".");?></div>
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <a class="small text-white stretched-link" href="siswa.php">View Details</a>
+                                    <div class="small text-white"><i class="ri-arrow-right-s-line"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-success text-white mb-4">
+                                <div class="card-body"><i class="ri-star-line"></i> Data Fasilitas : <?php echo number_format($jmlh_data_fasilitas,0,",",".");?></div>
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <a class="small text-white stretched-link" href="fasilitas.php">View Details</a>
+                                    <div class="small text-white"><i class="ri-arrow-right-s-line"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="row">
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-info text-white mb-4">
+                                <div class="card-body"><i class="ri-article-line"></i> Data Berita : <?php echo number_format($jmlh_data_berita,0,",",".");?></div>
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <a class="small text-white stretched-link" href="berita.php">View Details</a>
+                                    <div class="small text-white"><i class="ri-arrow-right-s-line"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-xl-3 col-md-6">
+                            <div class="card bg-secondary text-white mb-4">
+                                <div class="card-body"><i class="ri-gallery-line"></i> Data Galeri : <?php echo number_format($jmlh_data_galeri,0,",",".");?></div>
+                                <div class="card-footer d-flex align-items-center justify-content-between">
+                                    <a class="small text-white stretched-link" href="galeri.php">View Details</a>
+                                    <div class="small text-white"><i class="ri-arrow-right-s-line"></i></div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <!-- End: Summary -->
+                </div>
+                <!-- End: Content -->
+            </div>
+    </main>
+    <!-- End: Main -->
+
+    <!-- Start: JS -->
+    <script src="./assets/js/jquery.min.js"></script>
+    <script src="./assets/js/bootstrap.bundle.min.js"></script>
+    <script src="./assets/js/script.js"></script>
+    <!-- End: JS -->
+</body>
+
+</html>
